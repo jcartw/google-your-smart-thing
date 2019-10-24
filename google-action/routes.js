9bd77a90a210718c2ab37c6a2d42b7e134fb95d2
Justin Cartwright <justin.cartwright@technicity.io>
2019-10-24 10:18:31 -0300

    add handlers for EXECUTE and QUERY intents

--- google-action/routes.js
+++ google-action/routes.js
@@ -4,6 +4,7 @@ const config = require("../config/index");
 
 // cache
 let isUserLinked = true;
+let onOffState = false; // true = ON, false = OFF
 
 const app = smarthome({});
 module.exports.fulfill = app;
@@ -46,3 +47,76 @@ app.onDisconnect((body, headers) => {
     console.log("UNLINKED");
   }
 });
+
+// EXECUTE intent
+app.onExecute(async (body, headers) => {
+  console.log("EXECUTE");
+  const commands = [{ ids: [], status: "", states: { on: null } }];
+
+  for (const input of body.inputs) {
+    for (const command of input.payload.commands) {
+      for (const device of command.devices) {
+        for (const exec of command.execution) {
+          commands[0].ids.push(device.id);
+
+          let turnOn = null;
+          if (exec.command === "action.devices.commands.OnOff") {
+            turnOn = exec.params.on;
+          }
+
+          // send out command to device
+          if (turnOn !== null) {
+            commands[0].states.on = turnOn;
+            try {
+              _actuateLight({
+                deviceId: device.id,
+                turnOn
+              });
+              commands[0].status = "SUCCESS";
+            } catch (err) {
+              commands[0].status = "ERROR";
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return {
+    requestId: body.requestId,
+    payload: { commands }
+  };
+});
+
+// QUERY intent
+app.onQuery(async (body, headers) => {
+  console.log("QUERY");
+  const devices = {};
+
+  for (const input of body.inputs) {
+    for (const device of input.payload.devices) {
+      const on = _getLightOnOffState(device.id);
+      devices[device.id] = { on };
+    }
+  }
+
+  return {
+    requestId: body.requestId,
+    payload: { devices }
+  };
+});
+
+function _actuateLight({ deviceId, turnOn }) {
+  if (turnOn) {
+    console.log(`Turning Device ${deviceId} ON`);
+    onOffState = true;
+  } else {
+    console.log(`Turning Device ${deviceId} OFF`);
+    onOffState = false;
+  }
+}
+
+function _getLightOnOffState(deviceId) {
+  console.log(`Device ${deviceId} is ${onOffState ? "ON" : "OFF"}`);
+  return onOffState;
+}
